cdeb8b90b36db43817f1cfaab34350845cd061b6
Celaira <9338502+Celaira@users.noreply.github.com>
2020-01-08 15:54:36 -0600

    Added a '/users' route to the server.js file.

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const server = express();
 const setup = require('./config/setup.js');
 setup(server);
 
+const auth = require('./routes/authRoutes');
+
+server.use('/users', auth);
 
 server.get('/', (req, res) => {
     res.send({message: "Api up!"})
