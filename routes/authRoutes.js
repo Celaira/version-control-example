26dcc3fc8a0c61f527fb8eb70b2d50cede889db8
Celaira <9338502+Celaira@users.noreply.github.com>
2020-01-08 15:50:40 -0600

    Added Login and Register routes to the authRoutes file.

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,4 +2,41 @@ const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const generateToken = require("./generateToken");
 
-// TODO: Login && Register Routes
+const users = require('../data/models/usersModel')
+
+router.post("/register", (req, res) => {
+    let user = req.body;
+    const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
+    user.password = hash;
+  
+    users
+      .add(user)
+      .then(saved => {
+        res.status(201).json(saved);
+      })
+      .catch(error => {
+        console.error(error);
+        res.status(500).json(error);
+      });
+  });
+  
+  router.post("/login", (req, res) => {
+    let { username, password } = req.body;
+    users
+      .findByUsername({username})
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          const token = generateToken(user);
+          res.status(200).json({
+            id: user.id,
+            message: `Welcome ${user.username}`,
+            token,
+          });
+        } else {
+          res.status(401).json({ message: "Invalid Credentials" });
+        }
+      });
+  });
+  
+  module.exports = router;
