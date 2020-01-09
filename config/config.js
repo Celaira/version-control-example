de06ebe2fa1f3f4ee527c62ee55d60b35811cb13
Megan <megan.miller@smoldev.dev>
2020-01-09 14:00:06 -0600

    Adding insert functionality to the recipe model

--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
 module.exports = {
-    port: process.env.PORT,
-    jwtSecret: process.env.JWT_SECRET,
-    database_url: process.env.DB_URL,
-    dbEnv: process.env.DB_ENV,
-    dbName: process.env.DB_NAME,
-    dbUser: process.env.DB_USERNAME,
-}
+  port: process.env.PORT,
+  jwtSecret: process.env.JWT_SECRET,
+  database_url: process.env.DB_URL,
+  dbEnv: process.env.DB_ENV,
+  dbName: process.env.DB_NAME,
+  dbUser: process.env.DB_USERNAME
+};
--- data/models/recipesModel.js
+++ data/models/recipesModel.js
@@ -1,29 +1,47 @@
-const db = require('../dbConfig');
+const db = require("../dbConfig");
 
 module.exports = {
-    findById,
-    findByTitle,
-    add,
-    update,
-    remove
-}
+  findById,
+  findByTitle,
+  add,
+  update,
+  remove
+};
 
 function findById(id) {
-    return db('recipes').where(id)
+  return db("recipes").where(id);
 }
 
 function findByTitle(title) {
-    return db('recipes').where(title)
+  return db("recipes").where(title);
 }
 
 function add(recipe) {
-    // TODO: add code here
+  // TODO: add code here
+  const {
+      author_id,
+      title,
+      img,
+      prep_time,
+      cook_time,
+      notes,
+      instructions
+  }
+
+  db("recipes")
+    .insert(author_id, title, img, prep_time, cook_time, notes, "id")
+    .then(rec => {
+        console.log(rec)
+        const InstEntries = instructions.map((inst, i) => {
+            return { step_number: i +1, body: inst, recipe_id: rec.id }
+        })
+    });
 }
 
 function update(changes) {
-    // TODO: add code here
+  // TODO: add code here
 }
 
 function remove(id) {
-    // TODO: add code here
+  // TODO: add code here
 }
