29d082f3ed17a9c6c1e8f17c17af2f674045bd12
Megan <megan.miller@smoldev.dev>
2020-01-27 14:16:16 -0600

    Basic forking working, error with request.

--- data/models/recipesModel.js
+++ data/models/recipesModel.js
@@ -1,4 +1,4 @@
-const db = require("../dbConfig");
+const db = require("../dbConfig")
 
 module.exports = {
   find,
@@ -6,21 +6,20 @@ module.exports = {
   findByTitle,
   add,
   updateRecipe,
+  fork_recipe,
   remove
-};
+}
 
 function find() {
   return db("recipes as r")
     .join("instructions as i", "r.id", "=", "i.recipe_id")
-    .count("r.id")
-    .first();
-  // .select(
-  //   "r.*",
-  //   db.raw(
-  //     `json_agg(json_build_object('step', i.step_number, 'body', i.body)) as instructions`
-  //   )
-  // )
-  // .groupBy("r.id");
+    .select(
+      "r.*",
+      db.raw(
+        `json_agg(json_build_object('step', i.step_number, 'body', i.body)) as instructions`
+      )
+    )
+    .groupBy("r.id")
 }
 
 function findById(id) {
@@ -34,39 +33,39 @@ function findById(id) {
       )
     )
     .groupBy("r.id")
-    .first();
+    .first()
 }
 
 function findByTitle(title) {
-  return db("recipes").where(title);
+  return db("recipes").where(title)
 }
 
 function add(recipe) {
   // TODO: add code here
-  const { instructions, ...recipeEntry } = recipe;
+  const { instructions, ...recipeEntry } = recipe
 
   return db("recipes")
     .insert(recipeEntry, "id")
     .then(rec => {
-      const [id] = rec;
+      const [id] = rec
       const InstEntries = instructions.map((inst, i) => {
-        return { step_number: i + 1, body: inst.body, recipe_id: id };
-      });
+        return { step_number: i + 1, body: inst.body, recipe_id: id }
+      })
 
       return db("instructions")
         .insert(InstEntries)
         .then(inst_rec => {
-          return findById(id);
-        });
-    });
+          return findById(id)
+        })
+    })
 }
 
 function addInstruction(id, instructions) {
   const InstEntries = instructions.map((inst, i) => {
-    return { step_number: i + 1, body: inst.body, recipe_id: id };
-  });
+    return { step_number: i + 1, body: inst.body, recipe_id: id }
+  })
 
-  return db("instructions").insert(InstEntries);
+  return db("instructions").insert(InstEntries)
 }
 /*  Future Megan! Remove the transaction and figure out how to convert the recipe
      and instruction data into stringified json and add it to the edits table
@@ -78,35 +77,35 @@ function updateRecipe(id, changes) {
       prep_time: changes.prep_time,
       cook_time: changes.cook_time,
       notes: changes.notes
-    };
+    }
 
     const fullRecipe = {
       ...updatedRecipe,
       instructions: changes.instructions
-    };
+    }
 
     return trx("recipes")
       .where({ id })
       .update(updatedRecipe)
       .then(upInst => {
-        const insti = [];
-        let instiDel;
+        const insti = []
+        let instiDel
         changes.instructions.map((inst, i) => {
           if (!inst.step_number && !inst.body) {
-            instiDel = i + 1;
+            instiDel = i + 1
           } else if (!inst.step_number) {
             return insti.push({
               step_number: i + 1,
               body: inst.body,
               recipe_id: id
-            });
+            })
           } else {
             return trx("instructions")
               .where({ recipe_id: id })
               .andWhere({ step_number: inst.step_number })
-              .update({ body: inst.body });
+              .update({ body: inst.body })
           }
-        });
+        })
         return trx("instructions")
           .insert(insti)
           .then(ed => {
@@ -119,15 +118,15 @@ function updateRecipe(id, changes) {
                   owner_id: changes.author_id,
                   recipe_id: id,
                   changes: JSON.stringify(fullRecipe)
-                };
+                }
 
                 return trx("edits")
                   .insert(Edits)
-                  .then(eds => findById(id));
-              });
-          });
-      });
-  });
+                  .then(eds => findById(id))
+              })
+          })
+      })
+  })
 }
 
 // return db.transaction(trx => {
@@ -147,6 +146,34 @@ function updateRecipe(id, changes) {
 
 //   })
 // })
+
+function fork_recipe(recipe_id, info) {
+  const { author_id, forked_from } = info
+  return findById(recipe_id).then(res => {
+    const { instructions, ...recipe } = res
+    delete recipe.id
+    recipe.author_id = author_id
+    recipe.forked_from = forked_from
+
+    const forkedrec = {
+      ...recipe
+    }
+    return db("recipes")
+      .insert(forkedrec, "id")
+      .then(rec => {
+        const [id] = rec
+        const InstEntries = instructions.map((inst, i) => {
+          return { step_number: i + 1, body: inst.body, recipe_id: id }
+        })
+
+        return db("instructions")
+          .insert(InstEntries, "id")
+          .then(inst_rec => {
+            return id
+          })
+      })
+  })
+}
 
 function remove(id) {
   // TODO: add code here
--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -1,41 +1,48 @@
-const router = require("express").Router();
-const recipes = require("../data/models/recipesModel");
-const mid = require("../middleware/customMiddleware");
+const router = require("express").Router()
+const recipes = require("../data/models/recipesModel")
+const mid = require("../middleware/customMiddleware")
 
 router.get("/:id", (req, res) => {
   recipes.findById(req.params.id).then(x => {
-    res.status(200).json(x);
-  });
-});
+    res.status(200).json(x)
+  })
+})
 router.get("/", (req, res) => {
   recipes.find().then(x => {
-    res.status(200).json(x);
-  });
-});
+    res.status(200).json(x)
+  })
+})
+router.post("/:id", mid.restrict, (req, res) => {
+  const recipe = {
+    author_id: req.user.subject,
+    forked_from: req.params.id
+  }
+  recipes.fork_recipe(req.params.id, recipe)
+})
 
 router.post("/", mid.restrict, (req, res) => {
-  console.log(req.user);
+  console.log(req.user)
 
   let recipe = {
     author_id: req.user.subject,
     ...req.body
-  };
+  }
 
   recipes.add(recipe).then(saved => {
-    res.status(201).json(saved);
-  });
-});
+    res.status(201).json(saved)
+  })
+})
 
 router.put("/:id", mid.restrict, (req, res) => {
-  let { id } = req.params;
+  let { id } = req.params
   let changes = {
     author_id: req.user.subject,
     ...req.body
-  };
+  }
 
   recipes.updateRecipe(id, changes).then(saved => {
-    res.status(201).json(saved);
-  });
-});
+    res.status(201).json(saved)
+  })
+})
 
-module.exports = router;
+module.exports = router
